d4bdb0163b8adbd5887919806b3f5adcf0b9b0f1
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-06-21 17:22:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+function getFetch(){
+    let email=document.getElementById("email").value;
+      fetch(`https://api.eva.pingutil.com/email?email=${email}`)
+        .then(res => res.json())
+        .then(data => {
+            if(data.data.deliverable&&data.data.valid_syntax){
+                console.log('valid');
+                document.getElementById("validation").innerHTML=" ";
 
+            }else{
+                console.log('invalid');
+                document.getElementById("validation").innerHTML='Email invalid';
+                document.getElementById("validation").style.color="#ff0000"
+            }
+        })
+        .catch(error => console.log('error', error));
+}
